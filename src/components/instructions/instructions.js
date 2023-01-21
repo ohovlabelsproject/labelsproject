377cdd37123f2554f7c6183659a5ee2d4f90f933
John <john@johns-mbp.home>
2023-01-21 15:24:06 +0000

    Added instructions

--- src/components/instructions/instructions.js
+++ src/components/instructions/instructions.js
@@ -1,5 +1,11 @@
 function Instructions() {
-  return <section className="instructions">instructions</section>;
+  return (
+    <section className="instructions text-center">
+      <div>
+        <h1>Drag any labels you find stigmising (don't like) into the bin</h1>
+      </div>
+    </section>
+  );
 }
 
 export default Instructions;
