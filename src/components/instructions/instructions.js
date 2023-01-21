3f44446a082e563bfae57a845753e3920e5f4cac
John <john@johns-mbp.home>
2023-01-21 17:26:12 +0000

    Added padding to instructions

--- src/components/instructions/instructions.js
+++ src/components/instructions/instructions.js
@@ -2,7 +2,9 @@ function Instructions() {
   return (
     <section className="instructions text-center">
       <div>
-        <h1>Drag any labels you find stigmising (don't like) into the bin</h1>
+        <h1 className="px-1">
+          Drag labels you find stigmising (don't like) into the bin
+        </h1>
       </div>
     </section>
   );
