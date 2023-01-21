75387a94c16f6d8f31776276f738adec1a1a897b
John <john@johns-mbp.home>
2023-01-21 18:24:43 +0000

    Relabelled bin area

--- src/components/whiteboard/binzone/binzone.js
+++ src/components/whiteboard/binzone/binzone.js
@@ -1,5 +1,11 @@
 function BinZone() {
-  return <section className="binzone">Binzone</section>;
+  return (
+    <section className="binzone py-2">
+      <div className="binzone-droparea m-4">
+        <i className="fa fa-trash"></i>&nbsp;Bin
+      </div>
+    </section>
+  );
 }
 
 export default BinZone;
