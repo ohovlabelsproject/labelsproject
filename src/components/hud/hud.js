d45186d6cd1dc7be7e236e310ccb71c797d9b2cc
John <john@johns-mbp.home>
2023-01-21 15:05:36 +0000

    Add label creation button to HUD

--- src/components/hud/hud.js
+++ src/components/hud/hud.js
@@ -1,5 +1,17 @@
 function Hud() {
-  return <header className="hud"></header>;
+  return (
+    <header className="hud">
+      <div className="col-12 row">
+        <div className="col-4 hud-col-l">
+          <button className="btn btn-primary">
+            add label &nbsp;
+            <i className="fa fa-plus"></i>
+          </button>
+        </div>
+        <div className="col-8 hud-col-r">test text</div>
+      </div>
+    </header>
+  );
 }
 
 export default Hud;
