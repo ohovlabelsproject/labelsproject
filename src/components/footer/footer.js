2f61346fc12d55750d327117b5aa866862453a0e
John <john@johns-mbp.home>
2023-01-21 17:41:17 +0000

    Changed footer text

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,5 +1,9 @@
 function Footer() {
-  return <footer className="footer">footer</footer>;
+  return (
+    <footer className="footer">
+      <small>&copy; {new Date().getFullYear()} Our Hearings, Our Voice</small>
+    </footer>
+  );
 }
 
 export default Footer;
