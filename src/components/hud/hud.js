7daa74a934b803998a5bcfed35def7a17cb3210e
John <john@johns-mbp.home>
2023-01-23 00:37:44 +0000

    Restyle test

--- src/components/hud/hud.js
+++ src/components/hud/hud.js
@@ -3,7 +3,7 @@ function Hud() {
     <header className="hud">
       <div className="col-12 row">
         <div className="col-4 hud-col-l">
-          <button className="btn btn-primary">
+          <button className="btn-ohov-1">
             Add label &nbsp;
             <i className="fa fa-plus"></i>
           </button>
