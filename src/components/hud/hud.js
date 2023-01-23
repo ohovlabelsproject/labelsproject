06128afc30663a31f065ebbe1663609fc09d52d2
John <john@johns-mbp.home>
2023-01-23 12:07:43 +0000

    Testing small layout #7

--- src/components/hud/hud.js
+++ src/components/hud/hud.js
@@ -2,7 +2,7 @@ function Hud() {
   return (
     <header className="hud">
       <div className="col-12 row">
-        <div className="col-4 hud-col-l">
+        <div className="col-4 hud-col-l pl-2">
           <button className="btn-ohov-1">
             Add label &nbsp;
             <i className="fa fa-plus"></i>
@@ -10,6 +10,7 @@ function Hud() {
         </div>
         <div className="col-8 hud-col-r">
           <img
+            alt="Our Hearing, Our Voice logo"
             src="https://usercontent.one/wp/www.ohov.co.uk/wp-content/uploads/2019/08/OHOV_final_logo_colour-2.png"
             width="130px"
           />
