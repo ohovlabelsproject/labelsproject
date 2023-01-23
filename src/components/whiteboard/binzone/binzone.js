0f159028f66d7a468ba6eb845aba56ae6b4547f1
John <john@johns-mbp.home>
2023-01-23 13:24:45 +0000

    Implementing touchMove #3

--- src/components/whiteboard/binzone/binzone.js
+++ src/components/whiteboard/binzone/binzone.js
@@ -1,7 +1,7 @@
 function BinZone() {
   return (
     <section className="binzone py-2">
-      <div className="binzone-droparea m-4">
+      <div className="binzone-droparea m-4" id="binzone-droparea">
         <i className="fa fa-trash"></i>&nbsp;Bin
       </div>
     </section>
--- src/components/whiteboard/stickyzone/stickynote.js/stickynote.js
+++ src/components/whiteboard/stickyzone/stickynote.js/stickynote.js
@@ -5,17 +5,41 @@ function StickyNote(props) {
       id={`stickynote-${props.index}`}
       onTouchMove={(e) => {
         const touchLocation = e.targetTouches[0];
+        let stickyNote = document.getElementById(`stickynote-${props.index}`);
+        let stickyNoteRect = stickyNote.getBoundingClientRect();
+        let { pageX, pageY } = touchLocation;
+        // Allow sticky note to be dragged by its center:
+        stickyNote.style.position = "absolute";
+        stickyNote.style.left = pageX - stickyNoteRect.width / 2 + "px";
+        stickyNote.style.top = pageY - stickyNoteRect.height / 2 + "px";
+        // Stop page from scrolling/moving around while dragging sticky note:
+        document.body.classList.add("scroll-disable");
+      }}
+      onTouchEnd={() => {
+        let binDropZone = document.getElementById("binzone-droparea");
+        let binDropZoneRect = binDropZone.getBoundingClientRect();
+        let stickyNote = document.getElementById(`stickynote-${props.index}`);
+        let stickyNoteRect = stickyNote.getBoundingClientRect();
+        // The number of pixels out from boundary we'll allow users to be:
+        let boundaryTolerance = 20;
+        // Is the note vertically within the bin drop zone boundaries?
+        let isNoteWithinBinVertically =
+          stickyNoteRect.top >= binDropZoneRect.top - boundaryTolerance &&
+          stickyNoteRect.bottom <= binDropZoneRect.bottom + boundaryTolerance;
+        // Is the note horizontally within the bin drop zone boundaries?
+        let isNoteWithinBinHorizontally =
+          stickyNoteRect.left >= binDropZoneRect.left - boundaryTolerance &&
+          stickyNoteRect.right <= binDropZoneRect.right + boundaryTolerance;
+        // Is the note completely within the bin drop zone boundaries?
+        let isNoteWithinBin =
+          isNoteWithinBinVertically && isNoteWithinBinHorizontally;
 
-        document.getElementById(`stickynote-${props.index}`).style.position =
-          "absolute";
+        if (isNoteWithinBin) {
+          alert("in bin!");
+        }
 
-        document.getElementById(`stickynote-${props.index}`).style.left =
-          touchLocation.pageX + "px";
-
-        document.getElementById(`stickynote-${props.index}`).style.top =
-          touchLocation.pageY + "px";
-
-        //alert(`${touchLocation.pageX}, ${touchLocation.pageY}`);
+        // Allow scrolling now that sticky note is no longer being dragged:
+        document.body.classList.remove("scroll-disable");
       }}
     >
       {props.label}
