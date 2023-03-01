795cbde3032cd657ba12bf746e0945e3b9d34840
John <john@johns-mbp.home>
2023-03-01 17:11:00 +0000

    Trying to fix label submission display issue #2

--- src/components/modal/modal.js
+++ src/components/modal/modal.js
@@ -95,7 +95,7 @@ function ModalWindow(props) {
     document.getElementById("modal-label-submission").value = "";
     document.getElementById("app").style.marginTop = "0";
     document.getElementById("app").style.top = "0";
-    document.getElementById("app").style.window.scrollTo(0);
+    window.scrollTo(0);
     utils.device.orientation.update();
     document.getElementById("app").classList.remove("app");
     document.body.style.display = "none";
